29909650f9ab87fa3c1313667ad513b9fb331fe0
Mathéo <mathivanov.prog@icloud.com>
2024-12-11 17:02:11 +0100

    Ajout d'index.html et modification de tomagochi.js

--- tomagochi.js
+++ tomagochi.js
@@ -1,9 +1,9 @@
 class Animals {
     constructor(nom, energie=100, faim=0, bonheur=100) {
-        this.nom = nom
-        this.energie = energie
-        this.faim = faim
-        this.bonheur =bonheur
+        this.nom = nom;
+        this.energie = energie;
+        this.faim = faim;
+        this.bonheur =bonheur;
     };
     jouer() {
         this.energie -= 20;
@@ -33,26 +33,42 @@ class Animals {
     }
 };
 
-function user(titi) {
-    while(titi.estVivant()) {
-        alert("1 -> jouer; 2 -> manger; 3 -> dormir; 4 -> état de " + titi.nom);
-        choix = parseInt(prompt("Choisissez entre 1, 2, 3, 4 : "));
-        if(choix === 1) {
-            titi.jouer();
-        } else if( choix === 2) {
-            titi.manger();
-        } else if(choix === 3) {
-            titi.dormir()
-        } else if(choix === 4) {
-            titi.etat()
-        } else {
-            console.log("Vous devez entrer un nombre entre 1, 2, 3, 4 !!")
-            user(titi)
-        }
-    };
-    alert(titi.nom + " meurt");
-};
 
+function user(titi) {
+    const jours_total = 10;
+    for(let jour = 1; jour <= jours_total; jour++){
+        alert(`_Jours : ${jour}_`);
+        let i = 0;
+
+        while(titi.estVivant() && i < 3) {
+            let choix = parseInt(prompt(`Jour ${jour} : Choisissez une action pour ${titi.nom} : \n1 -> Jouer\n2 -> Manger\n3 -> Dormir\n4 -> Voir état`));
+            i += 1;
+            if(choix === 1) {
+                titi.jouer();
+            } else if( choix === 2) {
+                titi.manger();
+            } else if(choix === 3) {
+                titi.dormir()
+            } else if(choix === 4) {
+                titi.etat()
+            } else {
+                console.log("Vous devez entrer un nombre entre 1, 2, 3, 4 !!")
+                i--;
+                continue;
+            }
+            if(i % 3 === 0) {
+                alert(`${titi.nom} a ${titi.energie} d'énergie et un bonheur de ${titi.bonheur}`);
+            }
+        };
+
+        titi.energie -= 10;
+        titi.faim += 10;
+        titi.bonheur -= 5;
+        if(!titi.estVivant()) {alert(`${titi.nom} meurt au jour ${jour}`); break;};
+
+        console.log(`Fin du jour ${jour}`);
+    };
+};
 
 let titi = new Animals("titi");
 
